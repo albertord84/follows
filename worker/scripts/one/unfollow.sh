07f6125b2feca62f6b2b79d91864970b8bc9371c
iselaGit <imendozadec@gmail.com>
2018-07-10 17:28:50 -0300

    curls das sh do one - isela

--- worker/scripts/one/unfollow.sh
+++ worker/scripts/one/unfollow.sh
@@ -4,4 +4,6 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-wget -O /home/dumbuo5/public_html/follows/worker/log/unfollow-${date}.log   http://127.0.0.1:30080/follows/worker/scripts/unfollow.php
+curl http://127.0.0.1:30080/follows/worker/scripts/unfollow.php >> /home/dumbuo5/public_html/follows/worker/log/unfollow-${date}.log
+# wget -O /home/dumbuo5/public_html/follows/worker/log/unfollow-${date}.log   http://127.0.0.1:30080/follows/worker/scripts/unfollow.php
+
